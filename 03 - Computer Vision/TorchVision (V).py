a553130916e89dc47aba08ee822a0e02eed1c609
Jiwei-Cao <jiweicao2006@gmail.com>
2025-03-15 10:44:39 +0000

    Loading the Dataset

---
+++ 03 - Computer Vision/TorchVision (V).py
@@ -0,0 +1,28 @@
+import torch
+from torch import nn
+import torchvision
+from torchvision import datasets
+from torchvision import transforms
+from torchvision import ToTensor
+import matplotlib.pyplot as plt
+
+# 1. Getting a dataset
+train_data = datasets.FashionMNIST(
+    root = "data", 
+    train = True,
+    download =True,
+    transform = ToTensor(),
+    target_transform = None
+)
+
+test_data = datasets.FashionMNIST(
+    root = "data",
+    train = False,
+    download = True,
+    transform = ToTensor(),
+    target_transform = None
+)
+
+class_names = train_data.classes
+# print(class_names)
+
--- 03 - Computer Vision/TorchVision.py
+++
@@ -1,7 +0,0 @@
-import torch
-from torch import nn
-import torchvision
-from torchvision import datasets
-from torchvision import transforms
-from torchvision import ToTensor
-import matplotlib.pyplot as plt
