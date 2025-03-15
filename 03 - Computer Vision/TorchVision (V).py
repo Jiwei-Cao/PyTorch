71f4ab30604b76ab8c398d8b60c92f13bb4a8fd9
Jiwei-Cao <jiweicao2006@gmail.com>
2025-03-15 10:54:28 +0000

    Plotting the dataset

--- 03 - Computer Vision/TorchVision (V).py
+++ 03 - Computer Vision/TorchVision (V).py
@@ -28,5 +28,21 @@ class_names = train_data.classes
 
 image, label = train_data[0]
 # print(f"Image shape: {image.shape}")
-plt.imshow(image.squeeze())
+
+# plt.imshow(image.squeeze(), cmap = "gray")
+# plt.title(label)
+# plt.axis(False)
+# plt.show()
+
+torch.manual_seed(42)
+fig = plt.figure(figsize=(9,9))
+rows, cols = 4, 4
+for i in range(1, rows*cols+1):
+    random_idx = torch.randint(0, len(train_data), size=[1]).item()
+    img, label = train_data[random_idx]
+    fig.add_subplot(rows, cols, i)
+    plt.imshow(img.squeeze(), cmap = "gray")
+    plt.title(class_names[label])
+    plt.axis(False)
+
 plt.show()
