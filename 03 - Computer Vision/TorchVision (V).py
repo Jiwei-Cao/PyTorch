14c7833b221d731807c8b81c10b948e4690c788d
Jiwei-Cao <jiweicao2006@gmail.com>
2025-03-21 12:28:07 +0000

    Setting up Non-Linear Model

--- 03 - Computer Vision/TorchVision (V).py
+++ 03 - Computer Vision/TorchVision (V).py
@@ -224,4 +224,33 @@ model_0_results = eval_model(model=model_0,
                              loss_fn=loss_fn,
                              accuracy_fn=accuracy_fn)
 
-print(model_0_results)
+# print(model_0_results)
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# 6. Building a better model with non-linearity
+
+class FashionMNISTModelV1(nn.Module):
+    def __init__(self,
+                 input_shape: int,
+                 hidden_units: int, 
+                 output_shape: int):
+        super().__init__()
+        self.layer_stack = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=input_shape,
+                      out_features=hidden_units),
+            nn.ReLU(),
+            nn.Linear(in_features=hidden_units,
+                      out_features=output_shape),
+            nn.ReLU()
+        )
+
+    def forward(self, x: torch.Tensor):
+        return self.layer_stack(x)
+    
+torch.manual_seed(42) 
+model_1 = FashionMNISTModelV1(input_shape=784,
+                              hidden_units=10,
+                              output_shape=len(class_names)).to(device)
+
