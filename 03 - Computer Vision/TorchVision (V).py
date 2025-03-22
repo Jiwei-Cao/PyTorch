2e2ceca1b413dcb8fb3eabaa430f5c4c2e2bb555
Jiwei-Cao <jiweicao2006@gmail.com>
2025-03-22 15:05:51 +0000

    Building a CNN

--- 03 - Computer Vision/TorchVision (V).py
+++ 03 - Computer Vision/TorchVision (V).py
@@ -349,3 +349,38 @@ total_train_time_model_1 = print_train_time(start=train_time_start_on_model1,
                                             end=train_time_end_on_model1
                                             )
 
+# Model 2: Building a Convolutional Neural Network (CNN)
+
+class FashionMNISTModelV2(nn.module):
+    def __init__(self, input_shape: int, hidden_units: int, output_shape: int):
+        super().__init__()
+        self.conv_block_1 = nn.Sequential(
+            nn.Conv2d(in_channels=input_shape,
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units,
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2)
+        )
+        self.conv_block_2 = nn.Sequential(
+            nn.Conv2d(in_channels=hidden_units,
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units,
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d
+        )
