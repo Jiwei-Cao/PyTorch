1760a58e5b1267f05495784394195a38bbe24a2d
Jiwei-Cao <jiweicao2006@gmail.com>
2025-03-29 12:36:45 +0000

    Loading the data

--- 03 - Computer Vision/Computer Vision Model.py
+++ 03 - Computer Vision/Computer Vision Model.py
@@ -2,9 +2,11 @@ import torch
 import torchvision
 from torchvision import datasets
 from torchvision import transforms
+import matplotlib.pyplot as pyt
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
+# Load MNIST data
 train_data = datasets.MNIST(root=".",
                            train=True,
                            download=True,
@@ -17,3 +19,5 @@ test_data = datasets.MNIST(root=".",
 
 img = train_data[0][0]
 label = train_data[0][1]
+
+class_names = train_data.classes
