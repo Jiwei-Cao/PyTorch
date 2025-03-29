295be5acc7b0eb4010fd4cfd6071521e7ed268ed
Jiwei-Cao <jiweicao2006@gmail.com>
2025-03-29 12:55:30 +0000

    Creating the CNN model

--- 03 - Computer Vision/Computer Vision Model.py
+++ 03 - Computer Vision/Computer Vision Model.py
@@ -1,4 +1,5 @@
 import torch
+from torch import nn
 import torchvision
 from torchvision import datasets
 from torchvision import transforms
@@ -41,3 +42,50 @@ train_dataloader = DataLoader(dataset=train_data,
 test_dataloader = DataLoader(dataset=test_data,
                              batch_size=32,
                              shuffle=False)
+
+# Creating the model
+class MNIST_model(torch.nn.Module):
+    """Model capabale of predicting on MNIST dataset"""
+    def __init__(self, input_shape: int, hidden_units: int, output_shape: int):
+        super().__init__()
+        self.conv_block_1 = nn.Sequential(
+            nn.Conv2d(in_channels=input_shape,
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units,
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2) # Reduces the size of the featured map to highlight most important features
+        )
+        self.conv_block_2 = nn.Sequential(
+            nn.Conv2d(in_channels=hidden_units,
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units,
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2) # Reduces the size of the featured map to highlight most important features
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=hidden_units*7*7,
+                      out_features=output_shape)
+        )
+
+        def forward(self, x):
+            x = self.conv_block_1(x)
+            x = self.conv_block_2(x)
+            x = self.classifier(x)
+            return x
