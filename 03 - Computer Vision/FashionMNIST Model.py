79da718df86e67fa1f55e6ee48d6596ec803044e
Jiwei-Cao <jiweicao2006@gmail.com>
2025-03-29 14:05:16 +0000

    Creating model

--- 03 - Computer Vision/FashionMNIST Model.py
+++ 03 - Computer Vision/FashionMNIST Model.py
@@ -1,3 +1,5 @@
+import torch
+from torch import nn
 from torchvision import datasets
 from torchvision import transforms
 from torch.utils.data import DataLoader
@@ -24,3 +26,56 @@ fashion_mnist_test_dataloader = DataLoader(fashion_mnist_test,
                                            batch_size=32,
                                            shuffle=False)
 
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+# Creating the model
+class MNIST_model(torch.nn.Module):
+    """Model capabale of predicting on MNIST dataset"""
+    def __init__(self, input_shape: int, hidden_units: int, output_shape: int):
+        super().__init__()
+        self.conv_block_1 = nn.Sequential(
+            nn.Conv2d(in_channels=input_shape,
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units,
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2) # Reduces the size of the featured map to highlight most important features
+        )
+        self.conv_block_2 = nn.Sequential(
+            nn.Conv2d(in_channels=hidden_units,
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units,
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2) # Reduces the size of the featured map to highlight most important features
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=hidden_units*7*7,
+                      out_features=output_shape)
+        )
+
+    def forward(self, x):
+        x = self.conv_block_1(x)
+        x = self.conv_block_2(x)
+        x = self.classifier(x)
+        return x
+
+model = MNIST_model(input_shape=1,
+                    hidden_units=10,
+                    output_shape=10).to(device)
