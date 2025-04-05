cab9455f636cf77511da279c5c5a8a8cfacf1a12
Jiwei Cao <jiweicao2006@gmail.com>
2025-04-05 13:47:18 +0100

    Getting Data

--- 04 - Custom Datasets/Pizza, Steak and Sushi.py
+++ 04 - Custom Datasets/Pizza, Steak and Sushi.py
@@ -11,3 +11,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 from tqdm.auto import tqdm
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# 1. Get data
+data_path = Path("data/")
+image_path = data_path / "pizza_steak_sushi"
+
+# If the image folder doesn't exist, download it and prepare it
+if not image_path.is_dir():
+  image_path.mkdir(parents=True, exist_ok=True)
+
+# Download pizza, steak and sushi data images from github
+with open(data_path / "pizza_steak_sushi.zip", "wb") as f:
+  request = requests.get("https://github.com/mrdbourke/pytorch-deep-learning/raw/main/data/pizza_steak_sushi.zip")
+  f.write(request.content)
+
+# Unzip pizza, steak and sushi data
+with zipfile.ZipFile(data_path / "pizza_steak_sushi.zip", "r") as zip_ref:
+  zip_ref.extractall(image_path)
