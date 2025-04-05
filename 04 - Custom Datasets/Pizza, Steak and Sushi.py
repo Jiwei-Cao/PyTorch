8ab22124abe0ee07794ca35f927ecba6ba35ace3
Jiwei Cao <jiweicao2006@gmail.com>
2025-04-05 13:50:48 +0100

    Training Function

--- 04 - Custom Datasets/Pizza, Steak and Sushi.py
+++ 04 - Custom Datasets/Pizza, Steak and Sushi.py
@@ -167,3 +167,43 @@ class TinyVGG(nn.Module):
 
   def forward(self, x):
     return self.classifier(self.conv_block_2(self.conv_block_1(x)))
+
+# 4. Create training and testing functions for the model
+def train_step(model: torch.nn.Module,
+               dataloader: torch.utils.data.DataLoader,
+               loss_fn: torch.nn.Module,
+               optimizer: torch.optim.Optimizer):
+
+  model.train()
+
+  train_loss, train_acc = 0, 0
+
+  # Loop through dataloader and data batches
+  for batch, (X, y) in enumerate(dataloader):
+    X, y = X.to(device), y.to(device)
+
+    # 1. Forward pass
+    y_pred = model(X)
+    # print(y_pred)
+
+    # 2. Calculate and accumulate loss
+    loss = loss_fn(y_pred, y)
+    train_loss += loss.item()
+
+    # 3. Optimizer zero grad
+    optimizer.zero_grad()
+
+    # 4. Loss backward
+    loss.backward()
+
+    # 5. Optimizer step
+    optimizer.step()
+
+    # Calculate and accumualte accuracy metric across all batches
+    y_pred_class = torch.argmax(torch.softmax(y_pred, dim=1), dim=1)
+    train_acc += (y_pred_class == y).sum().item()/len(y_pred)
+
+  # Adjust metrics to get average loss and average accuracy per batch
+  train_loss = train_loss / len(dataloader)
+  train_acc = train_acc / len(dataloader)
+  return train_loss, train_acc
