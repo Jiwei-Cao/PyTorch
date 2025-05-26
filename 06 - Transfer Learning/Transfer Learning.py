fc7a9e3c3ca51a43b86340e58d641e662405b817
Jiwei-Cao <jiweicao2006@gmail.com>
2025-05-26 17:10:23 +0100

    Making Predictions on Images From the Test Set

--- 06 - Transfer Learning/Transfer Learning.py
+++ 06 - Transfer Learning/Transfer Learning.py
@@ -188,3 +188,54 @@ except:
 
 # Plot the loss curves of our model
 plot_loss_curves(results)
+
+# 6. Make predictions on images from the test set
+
+from typing import List, Tuple
+from PIL import Image
+
+# Take in a trained model
+def pred_and_plot_image(model: torch.nn.Module,
+                        image_path: str,
+                        class_names: List[str],
+                        image_size: Tuple[int, int] = (224, 224),
+                        transform: torchvision.transforms = None,
+                        device: torch.device=device):
+  
+  # Open the image with PIL
+  img = Image.open(image_path)
+
+  # Create a transform if one doesn't exist
+  if transform is not None:
+    image_transform = transform
+  else:
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+    image_transform = transforms.Compose([
+        transforms.Resize((224,224)),
+        transforms.ToTensor(),
+        normalize
+    ])
+
+  # Predict on image
+  model.to(device)
+
+  model.eval()
+  with torch.inference_mode():
+    # Transform the image and add an extra batch dimension (because we only deal with one image at a time)
+    transformed_image = image_transform(img).unsqueeze(dim=0) # [batch_size, color_channels, height, width]
+
+    # Make a prediction on the transformed image by passing it to the model 
+    target_image_pred = model(transformed_image.to(device))
+
+  # Convert the model's output logits to pred probs
+  target_image_pred_probs = torch.softmax(target_image_pred, dim=1)
+
+  # Convert the model's pred probs to pred labels
+  target_image_pred_label = torch.argmax(target_image_pred_probs, dim=1)
+
+  # Plot image with predicted label and probability
+  plt.figure()
+  plt.imshow(img)
+  plt.title(f"Pred: {class_names[target_image_pred_label]} | Prob: {target_image_pred_probs.max():.3f}")
+  plt.axis(False)
