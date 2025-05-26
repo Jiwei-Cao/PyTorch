dff46386fcd8276ed87d03ef38cf26374d9dc19c
Jiwei-Cao <jiweicao2006@gmail.com>
2025-05-26 12:08:31 +0100

    Imports

--- 06 - Transfer Learning/Transfer Learning.py
+++ 06 - Transfer Learning/Transfer Learning.py
@@ -3,3 +3,30 @@ import torchvision
 
 print(torch.__version__) 
 print(torchvision.__version__)
+
+# Continue with regular imports
+import matplotlib.pyplot as plt
+import torch
+import torchvision
+
+from torch import nn
+from torchvision import transforms
+
+# Try to get torchinfo, install it if it doesn't work
+try:
+    from torchinfo import summary
+except:
+    print("[INFO] Couldn't find torchinfo... installing it.")
+    !pip install -q torchinfo
+    from torchinfo import summary
+
+# Try to import the going_modular directory, download it from GitHub if it doesn't work
+try:
+    from going_modular.going_modular import data_setup, engine
+except:
+    # Get the going_modular scripts
+    print("[INFO] Couldn't find going_modular scripts... downloading them from GitHub.")
+    !git clone https://github.com/mrdbourke/pytorch-deep-learning
+    !mv pytorch-deep-learning/going_modular .
+    !rm -rf pytorch-deep-learning
+    from going_modular.going_modular import data_setup, engine
