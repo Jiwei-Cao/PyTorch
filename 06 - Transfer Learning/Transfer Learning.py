5a6fc00cff7fde5cddb1f225f01c80838c1382a7
Jiwei-Cao <jiweicao2006@gmail.com>
2025-05-28 10:00:51 +0100

    Fixing File Handling

--- 06 - Transfer Learning/Transfer Learning.py
+++ 06 - Transfer Learning/Transfer Learning.py
@@ -22,14 +22,14 @@ except:
 
 # Try to import the going_modular directory, download it from GitHub if it doesn't work
 try:
-    from going_modular.going_modular import data_setup, engine
+    from going_modular import data_setup, engine
 except:
     # Get the going_modular scripts
     print("[INFO] Couldn't find going_modular scripts... downloading them from GitHub.")
     # !git clone https://github.com/Jiwei-Cao/PyTorch
-    # !mv pytorch-deep-learning/going_modular .
-    # !rm -rf pytorch-deep-learning
-    from going_modular.going_modular import data_setup, engine
+    # !mv "PyTorch/05 - Going Modular/going_modular" .
+    # !rm -rf PyTorch
+    from going_modular import data_setup, engine
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 device
