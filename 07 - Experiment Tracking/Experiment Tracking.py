430e0489bc09d355f0e2b4cde50d2444214f0d14
Jiwei-Cao <jiweicao2006@gmail.com>
2025-05-29 12:16:25 +0100

    Regular Imports

--- 07 - Experiment Tracking/Experiment Tracking.py
+++ 07 - Experiment Tracking/Experiment Tracking.py
@@ -3,3 +3,30 @@ import torchvision
 
 print(torch.__version__)
 print(torchvision.__version__)
+
+# Regular imports
+import matplotlib.pyplot as plt
+import torch
+import torchvision
+
+from torch import nn
+from torchvision import transforms
+
+# Try to get torchinfo, install it if it doesn't work
+try:
+    from torchinfo import summary
+except:
+    print("[INFO] Couldn't find torchinfo... installing it.")
+    # !pip install -q torchinfo
+    from torchinfo import summary
+
+# Try to import the going_modular directory, download it from GitHub if it doesn't work
+try:
+    from going_modular import data_setup, engine
+except:
+    # Get the going_modular scripts
+    print("[INFO] Couldn't find going_modular scripts... downloading them from GitHub.")
+    # !git clone https://github.com/Jiwei-Cao/PyTorch
+    # !mv "PyTorch/05 - Going Modular/going_modular" .
+    # !rm -rf PyTorch
+    from going_modular import data_setup, engine
