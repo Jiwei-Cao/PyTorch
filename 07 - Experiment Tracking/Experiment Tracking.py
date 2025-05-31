4d23eae646e46195d7474b284fb90f307cc5fc6f
Jiwei-Cao <jiweicao2006@gmail.com>
2025-05-31 13:59:43 +0100

    Model Summary

--- 07 - Experiment Tracking/Experiment Tracking.py
+++ 07 - Experiment Tracking/Experiment Tracking.py
@@ -481,3 +481,12 @@ effnetb2_weights = torchvision.models.EfficientNet_B2_Weights.DEFAULT
 effnetb2 = torchvision.models.efficientnet_b2(weights=effnetb2_weights)
 
 effnetb2
+
+summary(
+    model=effnetb2,
+    input_size=(32, 3, 224, 224),
+    verbose=0,
+    col_names=["input_size", "output_size", "num_params", "trainable"],
+    col_width=20,
+    row_settings=["var_names"]
+)
