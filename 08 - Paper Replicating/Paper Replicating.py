ac65b8918d337574986c82f70abb5742143fc86e
Jiwei-Cao <jiweicao2006@gmail.com>
2025-06-04 15:56:51 +0100

    Regular Imports

--- 08 - Paper Replicating/Paper Replicating.py
+++ 08 - Paper Replicating/Paper Replicating.py
@@ -1,2 +1,29 @@
 import torch
 import torchvision
+
+# Regular imports
+import matplotlib.pyplot as plt
+
+from torch import nn
+from torchvision import transforms
+
+try:
+  from torchinfo import summary
+except:
+  print("[INFO] Couldn't find torchinfo... installing it")
+#   !pip install -q torchinfo
+  from torchinfo import summary
+
+# Import going_modular directory
+try:
+  from going_modular import data_setup, engine
+  from helper_functions import download_data, set_seeds, plot_loss_curves
+except:
+  # Get the going_modular scripts
+  print("[INFO] Couldn't find going_modular scripts... downloading them from github")
+#   !git clone https://github.com/Jiwei-Cao/PyTorch
+#   !mv "PyTorch/05 - Going Modular/going_modular" .
+#   !mv "PyTorch/helper_functions.py" .
+#   !rm -rf PyTorch
+  from going_modular import data_setup, engine
+  from helper_functions import download_data, set_seeds, plot_loss_curves
