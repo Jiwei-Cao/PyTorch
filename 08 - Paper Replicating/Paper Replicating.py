ce0a54b815770b7ba441ffef70103091228ddd3d
Jiwei-Cao <jiweicao2006@gmail.com>
2025-06-10 15:49:17 +0100

    Created the ViT Class

--- 08 - Paper Replicating/Paper Replicating.py
+++ 08 - Paper Replicating/Paper Replicating.py
@@ -486,3 +486,80 @@ summary(model=torch_transformer_encoder_layer,
         col_names=["input_size", "output_size", "num_params", "trainable"],
         col_width=20,
         row_settings=["var_names"])
+
+# ViT class
+class ViT(nn.Module):
+  def __init__(self,
+               img_size:int=224,
+               in_channels:int=3,
+               patch_size:int=16,
+               num_transformer_layers:int=12,
+               embedding_dim:int=768,
+               mlp_size:int=3072,
+               num_heads:int=12,
+               attn_dropout:int=0,
+               mlp_dropout:int=0.1,
+               embedding_dropout:int=0.1,
+               num_classes:int=1000):
+    super().__init__()
+
+    # Make an assertion that the image size is compatible with the patch size
+    assert img_size % patch_size == 0, f"Image size must be divisible by patch size, image; {img_size}, patch size: {patch_size}"
+
+    # Calculate the number of patches 
+    self.num_patches = (img_size * img_size) // patch_size**2
+
+    # Create learnable class embedding 
+    self.class_embedding = nn.Parameter(data=torch.rand(1, 1, embedding_dim),
+                                        requires_grad=True)
+    
+    # Create learnable position embedding
+    self.position_embedding = nn.Parameter(data=torch.randn(1, self.num_patches+1, embedding_dim))
+
+    # Create embedding dropout value
+    self.embedding_dropout = nn.Dropout(p=embedding_dropout)
+
+    # Create patch embedding layer
+    self.patch_embedding = PatchEmbedding(in_channels=in_channels,
+                                          patch_size=patch_size,
+                                          embedding_dim=embedding_dim)
+    
+    # Create the transformer encoder block
+    self.transformer_encoder = nn.Sequential(*[TransformerEncoderBlock(embedding_dim=embedding_dim,
+                                                                       num_heads=num_heads,
+                                                                       mlp_size=mlp_size,
+                                                                       mlp_dropout=mlp_dropout) for _ in range(num_transformer_layers)])
+
+    # Create classifier head
+    self.classifier = nn.Sequential(
+        nn.LayerNorm(normalized_shape=embedding_dim),
+        nn.Linear(in_features=embedding_dim,
+                  out_features=num_classes)
+    )    
+
+  def forward(self, x):
+    # Batch size
+    batch_size = x.shape[0]
+
+    # Create class token embedding and expand it to match the batch size
+    class_token = self.class_embedding.expand(batch_size, -1, -1)
+
+    # Create the patch embedding
+    x = self.patch_embedding(x)
+
+    # Concat class token embedding and patch embedding
+    x = torch.cat((class_token, x), dim=1)
+
+    # Add position embedding to class token and patch embedding
+    x = self.position_embedding + x
+
+    # Apply dropout to patch embedding
+    x = self.embedding_dropout(x)
+
+    # Pass position and patch embedding to transformer encoder
+    x = self.transformer_encoder(x)
+
+    # Put 0th index logit through classifier
+    x = self.classifier(x[:, 0])
+
+    return x
