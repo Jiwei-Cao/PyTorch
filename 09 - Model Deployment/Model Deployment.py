f0e4e4d7fe351680544910cdab3894f927afdc51
Jiwei-Cao <jiweicao2006@gmail.com>
2025-06-14 15:24:55 +0100

    Device Agnostic Code

--- 09 - Model Deployment/Model Deployment.py
+++ 09 - Model Deployment/Model Deployment.py
@@ -27,3 +27,6 @@ except:
 #   !rm -rf PyTorch
   from going_modular import data_setup, engine
   from helper_functions import download_data, set_seeds, plot_loss_curves
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+device
